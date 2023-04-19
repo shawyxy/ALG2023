87952bc49752cc69b30892c19cd591264e41bf93
shawyxy <9inghuan@gmail.com>
2023-04-19 22:59:22 +0800

    快排拾遗&&修正错误

--- yxc2023/Basic/Chapter01_BasicAlgorithm/class01.cpp
+++ yxc2023/Basic/Chapter01_BasicAlgorithm/class01.cpp
@@ -34,8 +34,11 @@ void QuitSortTest(vector<int>& arr, int L, int R)
 	// 缩小指针之间范围
 	while(i < j)
 	{
-		do i++; while(arr[i] < x);
-		do j--; while(arr[j] > x);
+		// do i++; while(arr[i] < x);
+		// do j--; while(arr[j] > x);
+		// 或
+		while(arr[++i] < x);
+		while(arr[--j] > x);
 		if(i < j) swap(arr[i], arr[j]); // 如果 i<j，说明这两个元素是一个逆序对
 	}
 	// 跳出循环，此时i==j，而且后面递归不用管已经被ij扫描过的元素(画图)
