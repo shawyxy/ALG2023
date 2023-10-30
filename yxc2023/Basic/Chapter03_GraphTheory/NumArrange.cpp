806a6d8bee56bdc18b3849f5a74deb9e47643dc4
shawyxy <9inghuan@gmail.com>
2023-10-30 20:16:34 +0800

    modify NumArrange

--- yxc2023/Basic/Chapter03_GraphTheory/NumArrange.cpp
+++ yxc2023/Basic/Chapter03_GraphTheory/NumArrange.cpp
@@ -8,6 +8,7 @@ int n;
 
 void dfs(int x)
 {
+    // 当填满时，打印
 	if (x == n)
 	{
 		for (int i = 0; i < n; i++) printf("    %d", path[i]);
@@ -16,12 +17,13 @@ void dfs(int x)
 	}
 	for (int i = 1; i <= n; i++)
 	{
+        // 如果这个结点还没有被访问过
 		if (visited[i] != true)
 		{
-			path[x] = i;
-			visited[i] = true;
-			dfs(x + 1);
-			visited[i] = false;
+			path[x] = i; // 记录到路径中
+			visited[i] = true; // 标记它被使用过
+			dfs(x + 1); // 递归下一层
+			visited[i] = false; // 递归返回后才能走到这一步，回溯还原现场
 		}
 	}
 }
@@ -30,7 +32,7 @@ int main()
 {
 	while (cin >> n)
 	{
-		dfs(0);
+		dfs(0); // 注意从第 0 个元素开始
 	}
 	return 0;
 }
