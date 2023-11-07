273eca449d76a746a846590171695c75f7352ad5
shawyxy <9inghuan@gmail.com>
2023-11-07 13:15:03 +0800

    TopoSort

--- yxc2023/Basic/Chapter03_GraphTheory/TopoSort.cpp
+++ yxc2023/Basic/Chapter03_GraphTheory/TopoSort.cpp
@@ -10,17 +10,18 @@ vector<int> ver[N], topo;
 bool TopoSort()
 {
 	queue<int> q;
-
+	// 将所有入度为0的顶点入队
 	for (int i = 1; i <= n; i++)
 		if (in[i] == 0) q.push(i);
 
 	while (!q.empty())
 	{
-
-		int x = q.top();
+		// 取出队头元素x(理论上可以取队列中任意元素)
+		int x = q.front();
 		q.pop();
 		topo.push_back(x);
-
+		// 将x顶点的出边(x, y)全部删除
+		// 当y顶点的入度为0,则入队
 		for (auto &y : ver[x])
 			if (--in[y] == 0) q.push(y);
 	}
@@ -30,21 +31,18 @@ bool TopoSort()
 
 int main()
 {
+	// 读入顶点数n,m行数据
 	cin >> n >> m;
 	for (int i = 0; i < m; i++)
 	{
 		int a, b;
 		cin >> a >> b;
+		// 读取(a, b)有向边, b的入度则+1
 		ver[a].push_back(b);
 		in[b]++;
 	}
 	if (!TopoSort()) puts("-1");
-	else 
-	{
-		for (int i = 0; i < topo.size() - 1; i++) printf("%d ", topo[i]);
-
-		cout << topo[topo.size() - 1];
-	}
+	else for (auto &e : topo) printf("%d ", e);
 
 	return 0;
 }
