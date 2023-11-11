399b6d3f9f2bd6665e4a7b33e520b9378ff0089d
shawyxy <9inghuan@gmail.com>
2023-11-11 17:15:22 +0800

    SPFA

--- yxc2023/Basic/Chapter03_GraphTheory/SPFA.cpp
+++ yxc2023/Basic/Chapter03_GraphTheory/SPFA.cpp
@@ -1,3 +1,5 @@
+// https://www.luogu.com.cn/problem/P3385
+
 #include <iostream>
 #include <queue>
 #include <cstring>
@@ -7,54 +9,63 @@ using namespace std;
 int n, m, s;
 const int N = 100010, M = 2 * N;
 int head[N], ver[M], edge[M], nxt[M], idx;
-int dist[N];
+int dist[N], cnt[N]; // cnt数组存储各个点入队的次数
 bool visited[N];
-queue<int> q;
+queue<int> q; // 用于存储待松弛的点
+
 
 void add(int x, int y, int z)
 {
-    idx++;
-    ver[idx] = y;
-    edge[idx] = z;
-    nxt[idx] = head[x];
-    head[x] = idx;
+	idx++;
+	ver[idx] = y;
+	edge[idx] = z;
+	nxt[idx] = head[x];
+	head[x] = idx;
 }
 
-void spfa(int s)
+bool spfa(int s) 
 {
+	memset(cnt, 0, sizeof(cnt)); 
 	memset(dist, 0x3f, sizeof(dist));
+	memset(visited, false, sizeof(visited));
 	dist[s] = 0;
-	q.push(s);
-	visited[s] = true;
-	while (!q.empty())
+	q.push(s);visited[s] = true; // 将源点入队，并标记为已访问
+	while (!q.empty()) // 当队列不为空时，循环执行
 	{
-		int x = q.front(); q.pop();
-		visited[x] = false;
-
-		for (int i = head[x]; i != 0; i = nxt[i])
+		int x = q.front(); q.pop(); visited[x] = false; // 取出队首元素x，并出队，标记为未访问
+		for (int i = head[x]; i != 0; i = nxt[i]) // 遍历以x为起点的所有边
 		{
-			int y = ver[i], z = edge[i];
-			if (dist[y] > dist[x] + z)
+			int y = ver[i], z = edge[i]; // y是边的终点，z是边的权值
+			if (dist[y] > dist[x] + z) // 如果可以通过x到y的边松弛y
 			{
-				dist[y] = dist[x] + z;
-				if (!visited[y])
-					q.push(y), visited[y] = true;
+				dist[y] = dist[x] + z; // 更新y的最短距离
+				if (++cnt[y] > n) return true; // 如果y入队的次数超过了n，说明存在负权环，返回true
+				if (!visited[y]) // 如果y不在队列中
+					q.push(y), visited[y] = true; // 将y入队，并标记为已访问
 			}
 		}
 	}
-	if (dist[n] == 0x3f3f3f3f)  puts("-1");
-	else for (int i = 1; i <= n; i++) cout << dist[i] << " ";
+	return false; // 如果没有发现负权环，返回false
 }
+
 
 int main()
 {
-	cin >> n >> m >> s;
-	for (int i = 0; i < m; i++)
+	int t;
+	cin >> t;
+	while (t--)
 	{
-		int x, y, z;
-		cin >> x >> y >> z;
-		add (x, y, z);
+		idx = 0, memset(head, 0, sizeof(head));
+		cin >> n >> m;
+		for (int i = 0; i < m; i++)
+		{
+			int u, v, w;
+			cin >> u >> v >> w;
+			add (u, v, w);
+			if (w >= 0) add (v, u, w);
+		}
+		if (spfa(1) == true) puts("YES");
+		else puts("NO");
 	}
-	spfa(s);
 	return 0;
 }
